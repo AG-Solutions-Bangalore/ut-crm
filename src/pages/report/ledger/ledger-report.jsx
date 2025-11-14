75f3b07dfe52dcb3b8ec20c09fd9bb40b1971227
Sajid-tech <s.khan9430319425@gmail.com>
2025-11-14 13:24:52 +0530

    ` fixed pdf issue `

--- src/pages/report/ledger/ledger-report.jsx
+++ src/pages/report/ledger/ledger-report.jsx
@@ -13,20 +13,21 @@ const { TabPane } = Tabs;
 const LedgerReport = () => {
   return (
     <div className="p-4 bg-white rounded-lg shadow-md">
-      <Tabs defaultActiveKey="1" type="card">
+      {/* <Tabs defaultActiveKey="1" type="card">
         <TabPane tab="Payable" key="1">
-        <UnifiedLedger />
-        </TabPane>
-
+    */}
+      <UnifiedLedger />
+        {/* </TabPane>
+   
         <TabPane tab="Receivables" key="2">
         
 
         <Ledger type="Receivables" />
         </TabPane>
-       
+        */}
 
         
-      </Tabs>
+      {/* </Tabs> */}
       
     </div>
   );
